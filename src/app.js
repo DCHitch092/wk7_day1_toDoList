39757bb2c0c3482334827100f4f7d0400a6fccf4
Richard <fahitch+github@gmail.com>
2019-11-25 16:03:13 +0000

    list has css and functionality

---
+++ src/app.js
@@ -0,0 +1,41 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data: {
+      toDos: [
+        {
+          toDoName: "Buy Cheese",
+          priority: "low"
+        },
+        {
+          toDoName: "Code This To Do App",
+          priority: "high"
+        }
+      ],
+      newToDo: "",
+      newPriority: ""
+    },
+    methods: {
+      saveToDoItem: function() {
+        this.toDos.push({
+          toDoName: this.newToDo,
+          priority: this.newPriority
+        });
+        this.newToDo = "";
+        this.newPriority = "";
+      }
+      // saveNewItem: function () {
+      //   this.items.push({
+      //     name: this.newItem,
+      //     isPurchased: false
+      //   });
+      //   this.newItem = "";
+      // },
+      // buyItem: function (index) {
+      //   this.items[index].isPurchased = true;
+      // }
+    }
+  });
+});
